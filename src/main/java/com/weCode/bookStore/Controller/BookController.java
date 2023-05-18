06ec11f9ecb0472e0734500ccfb7f9678891012c
this-is-sreehari <sreeharipathil@gmail.com>
2023-05-18 21:20:59 +0530

    first commit

---
+++ src/main/java/com/weCode/bookStore/Controller/BookController.java
@@ -0,0 +1,27 @@
+package com.weCode.bookStore.Controller;
+
+import com.weCode.bookStore.Dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks() {
+        BookDto book = BookDto.builder()
+                .title("My First Book Title")
+                .build();
+
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+
+    }
+}
